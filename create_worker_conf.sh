755c13d984841033487b57ddf161cfb8763265d6
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2022-07-31 15:52:54 -0300

    fix createworkerconf script and add run

--- create_worker_conf.sh
+++ create_worker_conf.sh
@@ -3,6 +3,5 @@
 VCPU=$1
 RAM=$2
 WORKER_ID=$3
-QUEUE_ID=$4
 
-echo "{\"vcpu\":${VCPU}, \"ram\":${RAM}, \"id\":\"${WORKER_ID}\", \"queue_id\":${QUEUE_ID}}" > ./worker-conf.json
+echo "{\"vcpu\":${VCPU}, \"ram\":${RAM}, \"id\":\"${WORKER_ID}\"}" > ./worker-conf.json
